e1d84a2482eaca8a65a434ef99b5f75cbf19e99f
haridevos01 <harikrishnan6500@gmail.com>
2026-03-02 12:41:29 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000
 
 app.use(cors({
     origin: ["http://localhost:5173",
-    "instapp-eight.vercel.app"],
+    "https://instapp-eight.vercel.app"],
     credentials: true
 }))
 
